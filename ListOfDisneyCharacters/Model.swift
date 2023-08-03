c2021dd14391d93e74d426d7cda795ac802b69d7
Ivan Sokol <sokolivan34@mail.ru>
2023-08-03 12:49:59 +0300

    Created model and network service.
    Cleaned project. Created model and network service.

---
+++ ListOfDisneyCharacters/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  ListOfDisneyCharacters
+//
+//  Created by Ваня Сокол on 03.08.2023.
+//
+
+import Foundation
---
+++ ListOfDisneyCharacters/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  ListOfDisneyCharacters
+//
+//  Created by Ваня Сокол on 03.08.2023.
+//
+
+import Foundation
