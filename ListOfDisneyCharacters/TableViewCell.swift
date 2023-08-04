584a776db5e3989f8be1bba5fbb169cac709de08
Ivan Sokol <sokolivan34@mail.ru>
2023-08-04 13:33:24 +0300

    Solving UI problem.

---
+++ ListOfDisneyCharacters/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  ListOfDisneyCharacters
+//
+//  Created by Ваня Сокол on 04.08.2023.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
